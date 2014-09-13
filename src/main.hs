94fca0e90da463a06c4e95e92cb7e3b987fde565
yulii <yuliinfo@gmail.com>
2014-09-13 16:59:58 +0900

    実行引数からMarkdown ファイルを読み込む #5

--- src/main.hs
+++ src/main.hs
@@ -3,12 +3,18 @@ import Network.Wai
 import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
 
+import System.Environment (getArgs)
+
 import qualified Text.Blaze.Html.Renderer.Utf8 as R
 import Text.Pandoc (readMarkdown, writeHtml, def)
 
 markdown = R.renderHtml . (writeHtml def) . (readMarkdown def)
 
-application _ respond = respond $
-  responseLBS status200 [("Content-Type", "text/html")] (markdown "# Markdown\nHello World")
+app _ respond = do
+  args <- getArgs
+  content <- readFile (head args)
+  respond $ responseLBS status200 [("Content-Type", "text/html")] (markdown content)
 
-main = run 3000 application
+main :: IO ()
+main = do
+  run 3000 app
