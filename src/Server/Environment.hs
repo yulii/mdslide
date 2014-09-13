2706a48da11418ebe1cd71639e127db0e9926f71
yulii <yuliinfo@gmail.com>
2014-09-13 17:44:52 +0900

    整理 #5

---
+++ src/Server/Environment.hs
@@ -0,0 +1,12 @@
+module Server.Environment
+  ( getContent
+  ) where
+
+import Control.Monad (liftM)
+import System.Environment (getArgs)
+
+getFileName :: IO String
+getFileName = liftM head getArgs
+
+getContent :: IO String
+getContent = getFileName >>= readFile
--- src/main.hs
+++ src/main.hs
@@ -1,20 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Network.Wai
-import Network.HTTP.Types (status200)
-import Network.Wai.Handler.Warp (run)
-
-import System.Environment (getArgs)
-
-import qualified Text.Blaze.Html.Renderer.Utf8 as R
-import Text.Pandoc (readMarkdown, writeHtml, def)
-
-markdown = R.renderHtml . (writeHtml def) . (readMarkdown def)
-
-app _ respond = do
-  args <- getArgs
-  content <- readFile (head args)
-  respond $ responseLBS status200 [("Content-Type", "text/html")] (markdown content)
+import Server
 
 main :: IO ()
 main = do
-  run 3000 app
+  server 3000
